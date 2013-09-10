c810b41521a606c36a1063bc3c54050ec928324f
quamis <quamis@gmail.com>
2013-09-10 22:13:39 +0300

    FIX: use hash() instead of the custom isEqual() function

--- pyBackup/worker/general.py
+++ pyBackup/worker/general.py
@@ -3,7 +3,9 @@ Created on Sep 7, 2013
 
 @author: lucian
 '''
-import os;
+import os
+import config.worklog
+
 
 class general(object):
     '''
@@ -19,6 +21,8 @@ class general(object):
         self.writer = writer
         self.fileComparer = fileComparer
         
+        self.worklog = config.worklog.worklog(os.path.join(self.writer.base(), "worklog.xml"))
+        
         
     def run(self):
         #print "\n".join(self.reader.getAll())
@@ -27,13 +31,15 @@ class general(object):
             pi = os.path.join(self.reader.base(), p)
             po = os.path.join(self.writer.base(), p)
             
-            if not self.fileComparer.isEqual(pi, po):
-                print "%s , %s" % (pi, po)
-                print "diff"
-                
+            hi = self.fileComparer.hash(pi)
+            ho = self.fileComparer.hash(po)
+            
+            if hi != ho:
+                print "%s" % (pi)
                 self.writer.copy(pi, po)
-                
+                self.worklog.append(pi, hi)
             else:
-                print "eq"
+                #print "eq"
+                pass
                 
-        
+        self.worklog.close()
