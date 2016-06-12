a8833dfaffa68c9f4d67d6b5baaa9fc92aec7988
Quamis <quamis@gmail.com>
2016-06-12 10:21:01 +0300

    added untracked file

---
+++ pyBackup/SourceReader/LocalPathReader/LocalPathReaderCached.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+'''
+Created on Sep 7, 2013
+@author: lucian
+'''
+from os import listdir
+from os.path import isdir, join, getatime, getmtime, getctime, getsize
+
+import SourceReader.Path as Path
+from SourceReader.LocalPathReader import LocalPathReader
+
+class LocalPathReaderCached(LocalPathReader.LocalPathReader):
+    def __init__(self):
+        self.db = None
+        super(LocalPathReaderCached, self).__init__()
+
+    def setCache(self, cache):
+        self.db = cache
+
+    def initialize(self):
+        print "LocalPathReaderCached.initialize %s" % (self.db.dbExists)
+        self.db.initialize()
+        print "LocalPathReaderCached.initialize %s" % (self.db.dbExists)
+        if self.db.dbExists:
+            self.paths = self.db.getAll()
+        else:
+            self.paths = self.goRecursivelly(self.basepath, [])
+    
+    def destroy(self):
+        self.db.destroy()
+        super(LocalPathReaderCached, self).destroy()
+    
+    def getNext(self):
+        #print "LocalPathReaderCached.getNext %s" % (self.db.dbExists)
+        
+        if self.db.dbExists:
+            ret = None
+            if self.index<len(self.paths):
+                p = self.paths[self.index]
+                ret = Path.Path(None, None)
+                ret.path = p[1]
+                ret.isDir = bool(p[2])
+                ret.ctime = int(p[3])
+                ret.mtime = float(p[4])
+                ret.size = int(p[5])
+                self.index+=1
+            return ret
+        else:
+            ret = super(LocalPathReaderCached, self).getNext()
+            if ret:
+                self.db.insertFileIntoFiles(ret, '')
+            return ret
+            
