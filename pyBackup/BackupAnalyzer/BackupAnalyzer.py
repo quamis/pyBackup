d7ab0f14d5e30312eaaa1f76570d73efc9ae385d
Quamis <quamis@gmail.com>
2016-06-14 21:26:51 +0300

    the analyzer works as expected

---
+++ pyBackup/BackupAnalyzer/BackupAnalyzer.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+'''
+Created on Sep 7, 2013
+@author: lucian
+'''
+import sqlite3
+import os, time
+
+class BackupAnalyzer(object):
+    def __init__(self):
+        self.cache = None
+        self.conn = None
+        
+    def setCache(self, cache):
+        self.cache = cache
+
+    def initialize(self):
+        self.conn = sqlite3.connect(self.cache.db)
+        c = self.conn.cursor()
+    
+    def destroy(self):
+        self.conn.commit()
+        self.conn.close()
+        
+    def getTotalCount(self):
+        c = self.conn.cursor()
+        c.execute('SELECT COUNT(*) FROM main.files AS fn WHERE NOT fn.isDir')
+        r = c.fetchone()
+        return r[0]
+        
+    def getTotalSize(self):
+        c = self.conn.cursor()
+        c.execute('SELECT SUM(size) FROM main.files AS fn WHERE NOT fn.isDir')
+        r = c.fetchone()
+        return r[0]
+        
+    def getAvgSize(self):
+        c = self.conn.cursor()
+        c.execute('SELECT SUM(size)/COUNT(*) FROM main.files AS fn WHERE NOT fn.isDir')
+        r = c.fetchone()
+        return r[0]
+        
+    def getDuplicatedFilesCount(self):
+        c = self.conn.cursor()
+        c.execute('SELECT COUNT(*) AS cnt FROM main.files AS fn WHERE NOT fn.isDir GROUP BY fn.hash HAVING cnt>1')
+        return sum(((cnt-1) for (cnt, ) in c.fetchall() if True))
+        
+    def getDuplicatedFilesSize(self):
+        c = self.conn.cursor()
+        c.execute('SELECT COUNT(*) AS cnt, size FROM main.files AS fn WHERE NOT fn.isDir GROUP BY fn.hash HAVING cnt>1')
+        return sum(((size*(cnt-1)) for (cnt, size) in c.fetchall() if True))
+        
+    def getEmptyFilesCount(self):
+        c = self.conn.cursor()
+        c.execute('SELECT COUNT(*) AS cnt FROM main.files AS fn WHERE NOT fn.isDir AND size=0 GROUP BY fn.hash')
+        r = c.fetchone()
+        return r[0]
+    
+    def getTop10LargestFiles(self):
+        c = self.conn.cursor()
+        c.execute('SELECT fn.size, fn.path FROM main.files AS fn WHERE NOT fn.isDir ORDER BY fn.size DESC LIMIT 10')
+        return c.fetchall()
+    
+    
--- pyBackup/test-BackupAnalyzer.py
+++ pyBackup/test-BackupAnalyzer.py
@@ -4,17 +4,28 @@
 '''
 
 import pprint
+import humanize
 from BackupAnalyzer.BackupAnalyzer import BackupAnalyzer
 import Cache.sqlite as sqlite
+import os
 
 pp = pprint.PrettyPrinter(indent=4)
 
-cacheNew = sqlite.sqlite();
-cacheNew.setCacheLocation('FileSystem.sqlite')
+cache = sqlite.sqlite();
+cache.setCacheLocation('FileSystem.sqlite')
 
 analyzer = BackupAnalyzer()
 analyzer.setCache(cache)
 analyzer.initialize()
 
+print "items total: %s files" % (analyzer.getTotalCount())
+print "size total: %s" % (humanize.naturalsize(analyzer.getTotalSize()))
+print "avg size: %s" % (humanize.naturalsize(analyzer.getAvgSize()))
+print "duplicated files total: %s files" % (analyzer.getDuplicatedFilesCount())
+print "duplicated files size: %s" % (humanize.naturalsize(analyzer.getDuplicatedFilesSize()))
+print "duplicated empty files : %s files" % (analyzer.getEmptyFilesCount())
+print "largest 10 files: \n       %s" % "\n       ".join((("%s (%s)" % (os.path.basename(path), humanize.naturalsize(size))) for (size, path) in analyzer.getTop10LargestFiles() if True))
+
 analyzer.destroy()
+
 
