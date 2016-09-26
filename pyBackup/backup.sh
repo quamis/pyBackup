7666e1ba98c38573782b9219b7171e031ca96dd0
Quamis <quamis@gmail.com>
2016-09-26 23:58:55 +0300

    cc

--- pyBackup/backup.sh
+++ pyBackup/backup.sh
@@ -8,20 +8,23 @@ echo "initialize"
 
 
 #SRC="/media/BIG/music/"; DST="/media/lucian/Backup/backups from 2016-09-21/music/";
+SRC="/media/BIG/spideroak/"; DST="/media/lucian/Backup/backups from 2016-09-21/spideroak/";
+
+
 
 #SRC="/cygdrive/d/musiq/dnb - bse podcasts/"; DST="test-data/DST/";
 #SRC="/cygdrive/d/exports/2015-08-27 -uTests/"; DST="test-data/DST/";
-SRC="/cygdrive/d/TFD tests/AIR uploader/"; DST="test-data/DST/";
+#SRC="/cygdrive/d/TFD tests/AIR uploader/"; DST="test-data/DST/";
 
 # -- simulate a fist-backup --
-rm -f "$SRC/backup.sqlite"; rm -f "$DST/backup.sqlite";
+#rm -f "$SRC/backup.sqlite"; rm -f "$DST/backup.sqlite";
 # -- test whole-copies, for empty backup DST's --
-rm -rf "$DST/"; mkdir -p "$DST";
+#rm -rf "$DST/"; mkdir -p "$DST";
 
 echo "calculate local hashes"
-python ./test-Hasher-FastContentHashV1Cached.py --verbose=4 --useCache=0 --data="$SRC" --cache="$SRC/backup.sqlite" 
+python ./test-Hasher-FastContentHashV1Cached.py --verbose=1 --useCache=0 --data="$SRC" --cache="$SRC/backup.sqlite" 
 
-exit;
+#exit;
 
 ##echo "compare"
 ##python ./test-Comparer.py --cacheNew="$SRC/backup.sqlite" --cacheOld="$DST/backup.sqlite" --doApply=0
@@ -46,4 +49,5 @@ echo "analize cache"
 python ./test-BackupAnalyzer.py --cache="$SRC/backup.sqlite" --data="$SRC"
 
 
+echo "copy cache"
 cp -f "$SRC/backup.sqlite" "$DST/backup.sqlite"
