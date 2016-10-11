475162186157207aaedac34f5e5a277fe9181efb
Lucian Sirbu <quamis@gmail.com>
2016-10-11 18:19:57 +0300

    cc

--- pyBackup/backup.demo.sh
+++ pyBackup/backup.demo.sh
@@ -5,6 +5,9 @@ SQLITE_DIR="/media/BIG/tmp/pyBackup.sqlite/";
 DST_DIR="/media/lucian/Backup/backups from 2016-09-21/";
 #DST_DIR="/media/BIG/tmp/pyBackup/";
 
+ACTION="$1";
+: ${ACTION:="backup"};
+
 
 if [ ! -d "$DST_DIR" ]; then
     error_exit "external hdd not mounted"
@@ -14,7 +17,7 @@ sync
 ########################################################################################################
 ## local backups #######################################################################################
 
-do_backup "projects" "$HOME/projects/"
+do_action "projects" "$HOME/projects/"
 
 sync
 ########################################################################################################
@@ -23,7 +26,7 @@ sync
 mkdir "$HOME/sshfs/" || error_exit "cannot mkdir"
 sshfs user@host:/ "$HOME/sshfs/" -p 22 || error_exit "cannot mount sshfs"
 
-do_backup "oc-v01" "$HOME/sshfs/media/mnt/a/1/2/3/4/"
+do_action "oc-v01" "$HOME/sshfs/media/mnt/a/1/2/3/4/"
 
 fusermount -u "$HOME/sshfs/" || error_exit "cannot disconnect from ssh"
 rmdir "$HOME/sshfs/"
